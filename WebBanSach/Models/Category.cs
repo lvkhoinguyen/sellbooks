94c45a3554237ae5a023bf4934e8474094248eee
levokhoinguyen.it@gmail.com <levokhoinguyen.it@gmail.com>
2026-09-01 23:44:36 +0700

    Initial commit: Project setup and CategoryController

---
+++ WebBanSach/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBanSach.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get;set;}
+        [Required]
+        public string Name { get;set;}
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
